a87f9cddb1e87c0b287b9b87e98671fdabedde66
agun36 <aguntoroti10@gmail.com>
2024-11-02 20:31:43 +0100

    feat: authform and favicon

--- src/components/authForm/authForm.tsx
+++ src/components/authForm/authForm.tsx
@@ -1,16 +1,23 @@
-import React from 'react'
-import logo from '../../assets/image/logo.svg'
-import pabloLogin from '../../assets/image/pablo-sign-in.svg'
-import UserForm from './userForm'
-import './authform.scss'
+import React from 'react';
+import logo from '../../assets/image/logo.svg';
+import pabloLogin from '../../assets/image/pablo-sign-in.svg';
+import UserForm from './userForm';
+import './authform.scss';
+
 const AuthForm: React.FC = () => {
-    return <section className="authform-user_form__details container">
-        <aside className="pablo-wrapper container">
-            <img src={logo} alt="showing the company name" className='pablo-wrapper-logo' />
-            <img src={pabloLogin} alt="" className='pablo-login' />
-        </aside>
-        <UserForm />
-    </section>
+    return (
+        <section className="authform-user__form-details container">
+            <div className='pablo-wrapper container'>
+                <div className='pablo-wrapper-logo'>
+                    <img className='pablo-wrapper-logo-img' src={logo} alt="showing the company name" />
+                </div>
+                <div className="flex-container">
+                    <img src={pabloLogin} alt="Login illustration" className='pablo-login' />
+                    <UserForm />
+                </div>
+            </div>
+        </section>
+    );
 }
 
-export default AuthForm
+export default AuthForm;
